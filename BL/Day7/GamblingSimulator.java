ba8a621cb48029c36acccf5cf5e53168ab42c5a9
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-24 15:26:25 +0530

    [yeswanth] Added UC3: Resign when stake reaches 50 or 150

--- BL/Day7/GamblingSimulator.java
+++ BL/Day7/GamblingSimulator.java
@@ -8,13 +8,14 @@ public class GamblingSimulator {
         int stake = 100;
         int bet = 1;
 
-        // UC2
-        double random = Math.random();
+        // UC3 (UC2 logic repeated inside loop)
+        while (stake > 50 && stake < 150) {
 
-        if (random < 0.5) {
-            stake = stake - bet;
-        } else {
-            stake = stake + bet;
+            if (Math.random() < 0.5) {
+                stake = stake - bet;
+            } else {
+                stake = stake + bet;
+            }
         }
 
     }
