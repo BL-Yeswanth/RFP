391069f7a1de262d08e76025dd4b2454d6596106
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-27 15:27:45 +0530

    [yeswanth] Added UC1: Calculate length of a line using Cartesian coordinates

--- BL/Day8/LineComparison.java
+++ BL/Day8/LineComparison.java
@@ -3,6 +3,16 @@ package Day8;
 public class LineComparison {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+
+        double x1 = 2;
+        double y1 = 3;
+        double x2 = 6;
+        double y2 = 7;
+
+        double length = Math.sqrt(
+                Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+        );
+
+        System.out.println("Length of the line = " + length);
     }
 }
