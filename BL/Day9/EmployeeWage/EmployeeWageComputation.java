00e7fe922febbfe0a62350fd1eb37f489c8a2ff9
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-28 11:30:35 +0530

    [yeswanth] Added UC3: Add part-time employee wage computation

--- BL/Day9/EmployeeWage/EmployeeWageComputation.java
+++ BL/Day9/EmployeeWage/EmployeeWageComputation.java
@@ -8,13 +8,19 @@ public class EmployeeWageComputation {
 
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 8;
 
-        int isPresent = (int) (Math.random() * 2); // 0 or 1
+        int empType = (int) (Math.random() * 3); // 0, 1, 2
         int dailyWage = 0;
 
-        if (isPresent == 1) {
+        if (empType == 1) {
             dailyWage = wagePerHour * fullDayHour;
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Full Time");
+
+        } else if (empType == 2) {
+            dailyWage = wagePerHour * partTimeHour;
+            System.out.println("Employee is Part Time");
+
         } else {
             System.out.println("Employee is Absent");
         }
