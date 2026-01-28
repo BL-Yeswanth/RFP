276058e5c4bcaa600c4be6b75b56605df0df2e52
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-28 11:24:43 +0530

    [yeswanth] Added UC1: Check employee attendance using random function

--- BL/Day9/EmployeeWage/EmployeeWageComputation.java
+++ BL/Day9/EmployeeWage/EmployeeWageComputation.java
@@ -5,5 +5,14 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC1: Check Employee Attendance
+        int isPresent = (int) (Math.random() * 2); // 0 or 1
+
+        if (isPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
