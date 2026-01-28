f819fddc642c82a09b68b96a08cc37a8257dff52
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-28 11:28:01 +0530

    [yeswanth] Added UC2: Calculate daily employee wage after attendance check

--- BL/Day9/EmployeeWage/EmployeeWageComputation.java
+++ BL/Day9/EmployeeWage/EmployeeWageComputation.java
@@ -6,13 +6,19 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        // UC1: Check Employee Attendance
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
         int isPresent = (int) (Math.random() * 2); // 0 or 1
+        int dailyWage = 0;
 
         if (isPresent == 1) {
+            dailyWage = wagePerHour * fullDayHour;
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
+
+        System.out.println("Daily Employee Wage = " + dailyWage);
     }
 }
