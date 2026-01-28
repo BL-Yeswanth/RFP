3cabaed57955612a8ab6665a47d91becf3e240e9
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-28 11:33:45 +0530

    [yeswanth] Added UC5: Calculate employee monthly wage for 20 working days

--- BL/Day9/EmployeeWage/EmployeeWageComputation.java
+++ BL/Day9/EmployeeWage/EmployeeWageComputation.java
@@ -9,26 +9,33 @@ public class EmployeeWageComputation {
         int wagePerHour = 20;
         int fullDayHour = 8;
         int partTimeHour = 8;
+        int workingDaysPerMonth = 20;
 
-        int empType = (int) (Math.random() * 3); // 0,1,2
-        int dailyWage = 0;
+        int totalMonthlyWage = 0;
 
-        switch (empType) {
+        // UC5: Calculate Monthly Wage
+        for (int day = 1; day <= workingDaysPerMonth; day++) {
 
-            case 1:
-                dailyWage = wagePerHour * fullDayHour;
-                System.out.println("Employee is Full Time");
-                break;
+            int empType = (int) (Math.random() * 3); // 0-Absent, 1-Full Time, 2-Part Time
+            int dailyWage = 0;
 
-            case 2:
-                dailyWage = wagePerHour * partTimeHour;
-                System.out.println("Employee is Part Time");
-                break;
+            switch (empType) {
 
-            default:
-                System.out.println("Employee is Absent");
+                case 1:
+                    dailyWage = wagePerHour * fullDayHour;
+                    break;
+
+                case 2:
+                    dailyWage = wagePerHour * partTimeHour;
+                    break;
+
+                default:
+                    dailyWage = 0;
+            }
+
+            totalMonthlyWage += dailyWage;
         }
 
-        System.out.println("Daily Employee Wage = " + dailyWage);
+        System.out.println("Total Monthly Employee Wage = " + totalMonthlyWage);
     }
 }
