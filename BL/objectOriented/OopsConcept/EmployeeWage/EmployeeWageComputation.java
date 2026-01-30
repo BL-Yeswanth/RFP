73fde0273fd803b9328c08a05f3e7a76f9c85435
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-30 12:50:21 +0530

    [yeswanth] Added UC2: Calculate daily employee wage based on attendance

--- BL/objectOriented/OopsConcept/EmployeeWage/EmployeeWageComputation.java
+++ BL/objectOriented/OopsConcept/EmployeeWage/EmployeeWageComputation.java
@@ -4,13 +4,16 @@ import java.util.Random;
 
 /**
  * Employee Wage Computation Program
- * UC1: Check Employee is Present or Absent
+ * UC1: Check Employee Attendance
+ * UC2: Calculate Daily Employee Wage
  */
 public class EmployeeWageComputation {
 
     // Constants
     public static final int IS_PRESENT = 1;
     public static final int IS_ABSENT = 0;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
 
     public static void main(String[] args) {
 
@@ -19,10 +22,16 @@ public class EmployeeWageComputation {
         Random random = new Random();
         int attendance = random.nextInt(2);
 
+        int dailyWage = 0;
+
         if (attendance == IS_PRESENT) {
             System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
         } else {
             System.out.println("Employee is Absent");
+            dailyWage = 0;
         }
+
+        System.out.println("Daily Employee Wage: " + dailyWage);
     }
 }
