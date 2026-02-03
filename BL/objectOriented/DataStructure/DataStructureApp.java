469e96336feb4f99b23cb182ecbd9e1caae05dcd
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-02-03 16:30:03 +0530

    [yeswanth] Added: Setup Data Structure Problems using Java Generics application

---
+++ BL/objectOriented/DataStructure/DataStructureApp.java
@@ -0,0 +1,9 @@
+package objectOriented.DataStructure;
+
+public class DataStructureApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Data Structure Problems using Java Generics");
+    }
+}
