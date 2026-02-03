22ba29b6085e70bb8f1339ddad41c6f7bdda0781
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-02-03 16:56:25 +0530

    [yeswanth] Added: UC1: Implement stack using linked list and push 56, 30, 70

--- BL/objectOriented/DataStructure/DataStructureApp.java
+++ BL/objectOriented/DataStructure/DataStructureApp.java
@@ -1,9 +1,66 @@
 package objectOriented.DataStructure;
 
+/**
+ * UC1: Ability to create a Stack of 56->30->70
+ * Using LinkedList
+ */
 public class DataStructureApp {
+
+    // Generic Stack using LinkedList
+    static class Stack<T> {
+
+        // Node class
+        private class Node {
+            T data;
+            Node next;
+
+            Node(T data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        private Node top;
+
+        /**
+         * Push operation (adds element to top of stack)
+         * Internally uses LinkedList add (addFirst)
+         */
+        public void push(T data) {
+            Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        /**
+         * Display stack elements
+         */
+        public void printStack() {
+            Node current = top;
+            System.out.print("Stack (Top -> Bottom): ");
+            while (current != null) {
+                System.out.print(current.data);
+                if (current.next != null) {
+                    System.out.print(" -> ");
+                }
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Data Structure Problems using Java Generics");
+
+        Stack<Integer> stack = new Stack<>();
+
+        // Push elements
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+
+        // Display stack
+        stack.printStack();
     }
 }
