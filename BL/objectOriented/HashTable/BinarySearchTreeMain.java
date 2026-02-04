9ed0fcec561e2d7b7e96d809ef77788c9408d645
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-02-04 14:22:27 +0530

    [yeswanth] Added: UC1: Create Binary Search Tree with root 56 and children 30, 70

--- BL/objectOriented/HashTable/BinarySearchTreeMain.java
+++ BL/objectOriented/HashTable/BinarySearchTreeMain.java
@@ -2,8 +2,79 @@ package objectOriented.HashTable;
 
 public class BinarySearchTreeMain {
 
+    interface INode<K extends Comparable<K>> {
+        K getKey();
+        void setLeft(INode<K> left);
+        void setRight(INode<K> right);
+        INode<K> getLeft();
+        INode<K> getRight();
+    }
+
+    static class MyBinaryNode<K extends Comparable<K>> implements INode<K> {
+        private K key;
+        private INode<K> left;
+        private INode<K> right;
+
+        MyBinaryNode(K key) {
+            this.key = key;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setLeft(INode<K> left) {
+            this.left = left;
+        }
+
+        public void setRight(INode<K> right) {
+            this.right = right;
+        }
+
+        public INode<K> getLeft() {
+            return left;
+        }
+
+        public INode<K> getRight() {
+            return right;
+        }
+    }
+
+    static class BinarySearchTree<K extends Comparable<K>> {
+        private INode<K> root;
+
+        public void add(K key) {
+            root = addRecursively(root, key);
+        }
+
+        private INode<K> addRecursively(INode<K> current, K key) {
+            if (current == null)
+                return new MyBinaryNode<>(key);
+
+            if (key.compareTo(current.getKey()) < 0)
+                current.setLeft(addRecursively(current.getLeft(), key));
+            else if (key.compareTo(current.getKey()) > 0)
+                current.setRight(addRecursively(current.getRight(), key));
+
+            return current;
+        }
+
+        public INode<K> getRoot() {
+            return root;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Binary Search Tree (BST) Problems");
+
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(56);
+        bst.add(30);
+        bst.add(70);
+
+        System.out.println("Root Node : " + bst.getRoot().getKey());
+        System.out.println("Left Child : " + bst.getRoot().getLeft().getKey());
+        System.out.println("Right Child : " + bst.getRoot().getRight().getKey());
     }
 }
