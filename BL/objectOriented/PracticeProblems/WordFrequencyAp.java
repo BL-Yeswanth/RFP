d34e4947f157a6baa1e4a38e826a33159cf41813
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-12 16:20:48 +0530

    Added UC2 : Add HashTable feature

--- BL/objectOriented/PracticeProblems/WordFrequencyAp.java
+++ BL/objectOriented/PracticeProblems/WordFrequencyAp.java
@@ -15,14 +15,14 @@ class MyLinkedList<K, V> {
 
     MyMapNode<K, V> head;
 
-    // Add or update word frequency
+    // Add or update node
     public void add(K key, V value) {
 
         MyMapNode<K, V> temp = head;
 
-        // Check if key already exists
         while (temp != null) {
 
+            // Update existing word frequency
             if (temp.key.equals(key)) {
                 temp.value = value;
                 return;
@@ -56,7 +56,7 @@ class MyLinkedList<K, V> {
         return null;
     }
 
-    // Display Linked List
+    // Display linked list
     public void display() {
 
         MyMapNode<K, V> temp = head;
@@ -72,33 +72,92 @@ class MyLinkedList<K, V> {
     }
 }
 
+class MyHashTable<K, V> {
+
+    int size;
+    MyLinkedList<K, V>[] table;
+
+    MyHashTable(int size) {
+
+        this.size = size;
+
+        table = new MyLinkedList[size];
+
+        // Create LinkedList for every index
+        for (int i = 0; i < size; i++) {
+            table[i] = new MyLinkedList<>();
+        }
+    }
+
+    // Generate index using hashcode
+    private int getIndex(K key) {
+
+        return Math.abs(key.hashCode()) % size;
+    }
+
+    // Add key-value pair
+    public void add(K key, V value) {
+
+        int index = getIndex(key);
+
+        table[index].add(key, value);
+    }
+
+    // Get value using key
+    public V get(K key) {
+
+        int index = getIndex(key);
+
+        return table[index].get(key);
+    }
+
+    // Display hash table
+    public void display() {
+
+        for (int i = 0; i < size; i++) {
+
+            if (table[i].head != null) {
+
+                System.out.println("Index " + i + ":");
+
+                table[i].display();
+
+                System.out.println();
+            }
+        }
+    }
+}
+
 public class WordFrequencyApp {
 
     public static void main(String[] args) {
 
-        String sentence = "To be or not to be";
+        String paragraph =
+                "Paranoids are not paranoid because they are paranoid " +
+                "but because they keep putting themselves deliberately " +
+                "into paranoid avoidable situations";
 
-        sentence = sentence.toLowerCase();
+        paragraph = paragraph.toLowerCase();
 
-        String[] words = sentence.split(" ");
+        String[] words = paragraph.split(" ");
 
-        MyLinkedList<String, Integer> list =
-                new MyLinkedList<>();
+        MyHashTable<String, Integer> hashTable =
+                new MyHashTable<>(10);
 
         // Count frequency
         for (String word : words) {
 
-            Integer count = list.get(word);
+            Integer count = hashTable.get(word);
 
             if (count == null) {
-                list.add(word, 1);
+                hashTable.add(word, 1);
             } else {
-                list.add(word, count + 1);
+                hashTable.add(word, count + 1);
             }
         }
 
         System.out.println("Word Frequencies:");
 
-        list.display();
+        hashTable.display();
     }
 }
