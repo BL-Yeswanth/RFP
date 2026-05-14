8b232df70f715ff333b8ed3891491cb3fb9e0f7f
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-14 15:33:35 +0530

    UC2: Added last name validation using regex

--- BL/AdvanceJava/UserRegistration.java
+++ BL/AdvanceJava/UserRegistration.java
@@ -11,21 +11,38 @@ public class UserRegistration {
         return Pattern.matches(regex, firstName);
     }
 
+    // Validate Last Name
+    public static boolean validateLastName(String lastName) {
+
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+        return Pattern.matches(regex, lastName);
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        // First Name
         System.out.println("Enter First Name:");
-
         String firstName = sc.nextLine();
 
         if (validateFirstName(firstName)) {
-
             System.out.println("Valid First Name");
         }
         else {
-
             System.out.println("Invalid First Name");
+        }
+
+        // Last Name
+        System.out.println("Enter Last Name:");
+        String lastName = sc.nextLine();
+
+        if (validateLastName(lastName)) {
+            System.out.println("Valid Last Name");
+        }
+        else {
+            System.out.println("Invalid Last Name");
         }
 
         sc.close();
