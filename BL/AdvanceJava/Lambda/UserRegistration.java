5dc55cb113f191dde35dbda45a9c07d0fe31a55b
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-18 15:22:16 +0530

    UC11: Added parameterized JUnit test for multiple email validations

--- BL/AdvanceJava/Lambda/UserRegistration.java
+++ BL/AdvanceJava/Lambda/UserRegistration.java
@@ -2,26 +2,6 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    // First Name Validation
-    public static boolean validateFirstName(
-            String firstName) {
-
-        String regex =
-                "^[A-Z][a-zA-Z]{2,}$";
-
-        return Pattern.matches(regex, firstName);
-    }
-
-    // Last Name Validation
-    public static boolean validateLastName(
-            String lastName) {
-
-        String regex =
-                "^[A-Z][a-zA-Z]{2,}$";
-
-        return Pattern.matches(regex, lastName);
-    }
-
     // Email Validation
     public static boolean validateEmail(
             String email) {
@@ -30,25 +10,5 @@ public class UserRegistration {
                 "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
 
         return Pattern.matches(regex, email);
-    }
-
-    // Mobile Validation
-    public static boolean validateMobileNumber(
-            String mobileNumber) {
-
-        String regex =
-                "^[0-9]{2}\\s[0-9]{10}$";
-
-        return Pattern.matches(regex, mobileNumber);
-    }
-
-    // Password Validation
-    public static boolean validatePassword(
-            String password) {
-
-        String regex =
-                "^(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&*!]*[@#$%^&*!][^@#$%^&*!]*$).{8,}$";
-
-        return Pattern.matches(regex, password);
     }
 }
