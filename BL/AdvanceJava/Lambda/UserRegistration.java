44a3f2bb896cb4393467c4fef8b1684aa740f345
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-18 16:16:36 +0530

    UC13: Refactored user validation using lambda functions

--- BL/AdvanceJava/Lambda/UserRegistration.java
+++ BL/AdvanceJava/Lambda/UserRegistration.java
@@ -2,113 +2,81 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    // First Name Validation
-    public static boolean validateFirstName(
-            String firstName)
-            throws UserRegistrationException {
+    // Lambda Validation - First Name
+    static UserValidation firstNameValidator =
+            firstName ->
+                    Pattern.matches(
+                            "^[A-Z][a-zA-Z]{2,}$",
+                            firstName
+                    );
 
-        String regex =
-                "^[A-Z][a-zA-Z]{2,}$";
+    // Lambda Validation - Last Name
+    static UserValidation lastNameValidator =
+            lastName ->
+                    Pattern.matches(
+                            "^[A-Z][a-zA-Z]{2,}$",
+                            lastName
+                    );
 
-        if (Pattern.matches(regex, firstName)) {
+    // Lambda Validation - Email
+    static UserValidation emailValidator =
+            email ->
+                    Pattern.matches(
+                            "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$",
+                            email
+                    );
 
-            return true;
-        }
+    // Lambda Validation - Mobile Number
+    static UserValidation mobileValidator =
+            mobile ->
+                    Pattern.matches(
+                            "^[0-9]{2}\\s[0-9]{10}$",
+                            mobile
+                    );
 
-        throw new UserRegistrationException(
-                UserRegistrationException
-                        .ExceptionType
-                        .INVALID_FIRST_NAME,
+    // Lambda Validation - Password
+    static UserValidation passwordValidator =
+            password ->
+                    Pattern.matches(
+                            "^(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&*!]*[@#$%^&*!][^@#$%^&*!]*$).{8,}$",
+                            password
+                    );
 
-                "Invalid First Name"
+    public static void main(String[] args) {
+
+        System.out.println(
+                "First Name : "
+                        +
+                        firstNameValidator
+                                .validate("Yaswanth")
         );
-    }
 
-    // Last Name Validation
-    public static boolean validateLastName(
-            String lastName)
-            throws UserRegistrationException {
-
-        String regex =
-                "^[A-Z][a-zA-Z]{2,}$";
-
-        if (Pattern.matches(regex, lastName)) {
-
-            return true;
-        }
-
-        throw new UserRegistrationException(
-                UserRegistrationException
-                        .ExceptionType
-                        .INVALID_LAST_NAME,
-
-                "Invalid Last Name"
+        System.out.println(
+                "Last Name : "
+                        +
+                        lastNameValidator
+                                .validate("Polisetti")
         );
-    }
 
-    // Email Validation
-    public static boolean validateEmail(
-            String email)
-            throws UserRegistrationException {
-
-        String regex =
-                "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
-
-        if (Pattern.matches(regex, email)) {
-
-            return true;
-        }
-
-        throw new UserRegistrationException(
-                UserRegistrationException
-                        .ExceptionType
-                        .INVALID_EMAIL,
-
-                "Invalid Email"
+        System.out.println(
+                "Email : "
+                        +
+                        emailValidator
+                                .validate("abc.xyz@bl.co.in")
         );
-    }
 
-    // Mobile Number Validation
-    public static boolean validateMobileNumber(
-            String mobileNumber)
-            throws UserRegistrationException {
-
-        String regex =
-                "^[0-9]{2}\\s[0-9]{10}$";
-
-        if (Pattern.matches(regex, mobileNumber)) {
-
-            return true;
-        }
-
-        throw new UserRegistrationException(
-                UserRegistrationException
-                        .ExceptionType
-                        .INVALID_MOBILE_NUMBER,
-
-                "Invalid Mobile Number"
+        System.out.println(
+                "Mobile Number : "
+                        +
+                        mobileValidator
+                                .validate("91 9919819801")
         );
-    }
 
-    // Password Validation
-    public static boolean validatePassword(
-            String password)
-            throws UserRegistrationException {
-
-        String regex =
-                "^(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&*!]*[@#$%^&*!][^@#$%^&*!]*$).{8,}$";
-
-        if (Pattern.matches(regex, password)) {
-
-            return true;
-        }
-
-        throw new UserRegistrationException(
-                UserRegistrationException
-                        .ExceptionType
-                        .INVALID_PASSWORD,
-
-                "Invalid Password"
+        System.out.println(
+                "Password : "
+                        +
+                        passwordValidator
+                                .validate("Password1@")
         );
     }
 }
---
+++ BL/AdvanceJava/Lambda/UserValidation.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+public interface UserValidation {
+
+    boolean validate(String value);
+}
