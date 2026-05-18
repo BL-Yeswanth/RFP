e23ce8e0a5c18eafef366520cf4be560f7ba38d0
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-18 14:35:45 +0530

    UC3: Added validation for email using regex

--- BL/AdvanceJava/Lambda/UserRegistration.java
+++ BL/AdvanceJava/Lambda/UserRegistration.java
@@ -28,16 +28,33 @@ public class UserRegistration {
         );
     }
 
+    // Validate Email
+    public static boolean validateEmail(
+            String email) {
+
+        String regex =
+                "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
+
+        return Pattern.matches(
+                regex,
+                email
+        );
+    }
+
     public static void main(String[] args) {
 
         String firstName = "Yaswanth";
         String lastName = "Polisetti";
+        String email = "abc.xyz@bl.co.in";
 
         boolean firstNameResult =
                 validateFirstName(firstName);
 
         boolean lastNameResult =
                 validateLastName(lastName);
+
+        boolean emailResult =
+                validateEmail(email);
 
         if (firstNameResult) {
 
@@ -62,6 +79,19 @@ public class UserRegistration {
 
             System.out.println(
                     "Invalid Last Name"
+            );
+        }
+
+        if (emailResult) {
+
+            System.out.println(
+                    "Valid Email"
+            );
+
+        } else {
+
+            System.out.println(
+                    "Invalid Email"
             );
         }
     }
