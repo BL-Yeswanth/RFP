9c18d5aa222f3b60f32bd5e661784d2ca031df1a
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-18 15:07:18 +0530

    UC7: Added password validation for at least one numeric character

--- BL/AdvanceJava/Lambda/UserRegistration.java
+++ BL/AdvanceJava/Lambda/UserRegistration.java
@@ -61,10 +61,11 @@ public class UserRegistration {
         /*
          Rule 1 -> Minimum 8 Characters
          Rule 2 -> At least 1 Upper Case
+         Rule 3 -> At least 1 Numeric Number
         */
 
         String regex =
-                "^(?=.*[A-Z]).{8,}$";
+                "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 
         return Pattern.matches(
                 regex,
@@ -78,7 +79,7 @@ public class UserRegistration {
         String lastName = "Polisetti";
         String email = "abc.xyz@bl.co.in";
         String mobileNumber = "91 9919819801";
-        String password = "Password";
+        String password = "Password1";
 
         boolean firstNameResult =
                 validateFirstName(firstName);
