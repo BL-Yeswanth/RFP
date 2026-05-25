9f0aa3c42a8c1c4fe8bbdf5b866a345d8470e0ec
yeswanth-polisetti <yeswanth.polisetti@bridgelabz.com>
2026-05-25 17:26:54 +0530

    UC11: Added sorting contacts alphabetically using Java streams

--- BL/JavaIO/IoStreamsDay28/AddressBook.java
+++ BL/JavaIO/IoStreamsDay28/AddressBook.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class AddressBook {
 
@@ -13,9 +14,15 @@ public class AddressBook {
         contactList.add(contact);
     }
 
-    // Get Contact List
-    public ArrayList<Contact> getContacts() {
+    // Sort Contacts By Name
+    public void sortByName() {
 
-        return contactList;
+        contactList.stream()
+                .sorted(
+                        Comparator.comparing(
+                                contact -> contact.firstName
+                        )
+                )
+                .forEach(System.out::println);
     }
 }
--- BL/JavaIO/IoStreamsDay28/AddressBookMain.java
+++ BL/JavaIO/IoStreamsDay28/AddressBookMain.java
@@ -1,7 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class AddressBookMain {
 
     public static void main(String[] args) {
@@ -10,15 +6,11 @@ public class AddressBookMain {
                 "Welcome To Address Book Program"
         );
 
-        // Dictionary Of Address Books
-        Map<String, AddressBook> addressBookMap =
-                new HashMap<>();
-
-        // Family Address Book
-        AddressBook familyBook =
+        AddressBook addressBook =
                 new AddressBook();
 
-        familyBook.addContact(
+        // Add Contacts
+        addressBook.addContact(
                 new Contact(
                         "Yaswanth",
                         "Polisetti",
@@ -31,121 +23,37 @@ public class AddressBookMain {
                 )
         );
 
-        familyBook.addContact(
+        addressBook.addContact(
                 new Contact(
-                        "Kiran",
+                        "Arun",
                         "Kumar",
                         "XYZ Street",
                         "Hyderabad",
                         "Telangana",
                         "500001",
                         "9876501234",
-                        "kiran@gmail.com"
-                )
-        );
-
-        addressBookMap.put(
-                "Family",
-                familyBook
-        );
-
-        // Friends Address Book
-        AddressBook friendsBook =
-                new AddressBook();
-
-        friendsBook.addContact(
-                new Contact(
-                        "Rahul",
-                        "Sharma",
-                        "MG Road",
-                        "Chennai",
-                        "Tamil Nadu",
-                        "600002",
-                        "9988776655",
-                        "rahul@gmail.com"
-                )
-        );
-
-        friendsBook.addContact(
-                new Contact(
-                        "Arun",
-                        "Reddy",
-                        "Anna Nagar",
-                        "Hyderabad",
-                        "Telangana",
-                        "500002",
-                        "9871234567",
                         "arun@gmail.com"
                 )
         );
 
-        addressBookMap.put(
-                "Friends",
-                friendsBook
+        addressBook.addContact(
+                new Contact(
+                        "Kiran",
+                        "Reddy",
+                        "MG Road",
+                        "Bangalore",
+                        "Karnataka",
+                        "560001",
+                        "9988776655",
+                        "kiran@gmail.com"
+                )
         );
 
-        // Count By City
-        Map<String, Long> cityCount =
-                addressBookMap.values()
-                        .stream()
-                        .flatMap(
-                                addressBook ->
-                                        addressBook
-                                                .getContacts()
-                                                .stream()
-                        )
-                        .collect(
-                                Collectors.groupingBy(
-                                        contact -> contact.city,
-                                        Collectors.counting()
-                                )
-                        );
-
-        // Count By State
-        Map<String, Long> stateCount =
-                addressBookMap.values()
-                        .stream()
-                        .flatMap(
-                                addressBook ->
-                                        addressBook
-                                                .getContacts()
-                                                .stream()
-                        )
-                        .collect(
-                                Collectors.groupingBy(
-                                        contact -> contact.state,
-                                        Collectors.counting()
-                                )
-                        );
-
-        // Display Count By City
+        // Display Sorted Contacts
         System.out.println(
-                "\nCount By City"
+                "\nContacts Sorted Alphabetically"
         );
 
-        cityCount.forEach(
-                (city, count) ->
-
-                        System.out.println(
-                                city
-                                        + " : "
-                                        + count
-                        )
-        );
-
-        // Display Count By State
-        System.out.println(
-                "\nCount By State"
-        );
-
-        stateCount.forEach(
-                (state, count) ->
-
-                        System.out.println(
-                                state
-                                        + " : "
-                                        + count
-                        )
-        );
+        addressBook.sortByName();
     }
 }
